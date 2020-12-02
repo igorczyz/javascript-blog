27416e7982800693fcf5b556446388ab117619bb
Igor Czyz <igorczyz13@gmail.com>
2020-12-02 09:24:44 +0100

    Add .eslintrc.json

--- js/script.js
+++ js/script.js
@@ -9,38 +9,27 @@ const titleClickHandler = function (event) {
 
 
   /* [DONE] remove class 'active' from all article links  */
-
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
   /* [DONE] add class 'active' to the clicked link */
-
   console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
-
   /* [DONE] remove class 'active' from all articles */
-
   const activeArticles = document.querySelectorAll('.posts article.active');
 
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
-
   /* [DONE] get 'href' attribute from the clicked link */
-
   const articleSelector = clickedElement.getAttribute('href');
-
-
   /* [DONE] find the correct article using the selector (value of 'href' attribute) */
-
   const targetArticle = document.querySelector(articleSelector);
-
   /* [DONE] add class 'active' to the correct article */
-
   targetArticle.classList.add('active');
-}
+};
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
@@ -49,12 +38,9 @@ const optArticleSelector = '.post',
 function generateTitleLinks() {
 
   /* [DONE] remove contents of titleList */
-
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
-
   /* [DONE] find all the articles and save them to varibale: articles */
-
   const articles = document.querySelectorAll(optArticleSelector);
 
   let html = '';
@@ -67,7 +53,7 @@ function generateTitleLinks() {
     /* [DONE] create HTML of the link */
     const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
     /* [DONE] insert link into titleList */
-    html = html + linkHTML
+    html = html + linkHTML;
   }
   titleList.innerHTML = html;
 }
@@ -75,7 +61,7 @@ function generateTitleLinks() {
 generateTitleLinks();
 
 const links = document.querySelectorAll('.titles a');
-console.log('links')
+console.log('links');
 for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
